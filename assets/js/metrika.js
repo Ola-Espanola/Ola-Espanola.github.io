6603495ce57767cd011263e0e90d3b8fbb461259
alexey-s-potapov <103932994+alexey-s-potapov@users.noreply.github.com>
2026-09-26 20:44:07 +0200

    Update Yandex Metrica tag initialization

--- assets/js/metrika.js
+++ assets/js/metrika.js
@@ -32,17 +32,22 @@
     (m[i].a = m[i].a || []).push(arguments);
   };
   m[i].l = 1 * new Date();
+  for (var j = 0; j < document.scripts.length; j++) {
+    if (document.scripts[j].src === r) return;
+  }
   k = e.createElement(t);
   a = e.getElementsByTagName(t)[0];
   k.async = 1;
   k.src = r;
   a.parentNode.insertBefore(k, a);
-})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=112065150", "ym");
 
 ym(112065150, "init", {
+  ssr: true,
+  webvisor: true,
   clickmap: true,
-  trackLinks: true,
-  accurateTrackBounce: true
+  accurateTrackBounce: true,
+  trackLinks: true
 });
 
 // Tax calculator interaction guard.
